11a93c6a934389f8abbc6c76b3b47dae2e88c19d
Jack Shi <shisoftgenius@gmail.com>
2017-08-11 01:38:02 +0800

    new map function

--- src/expr/symbols/stream.rs
+++ src/expr/symbols/stream.rs
@@ -1,11 +1,29 @@
 use super::*;
-use super::lambda::eval_lambda;
+use super::functions::eval_function;
+use types::Value;
 
 pub fn map(mut exprs: Vec<SExpr>) -> Result<SExpr, String> {
-    let func = exprs.remove(0);
-    let mut result = Vec::with_capacity(exprs.len());
-    for val in exprs {
-        result.push(eval_lambda(&func, vec![val])?);
+    let func = exprs.pop().unwrap();
+    let data = exprs.pop().unwrap();
+    let mut result;
+    match data {
+        SExpr::Value(Value::Array(arr)) => {
+            result = Vec::with_capacity(arr.len());
+            for val in arr {
+                result.push(eval_function(
+                    &func,
+                    vec![SExpr::Value(val)])?);
+            }
+        },
+        SExpr::Vec(expr_list) => {
+            result = Vec::with_capacity(expr_list.len());
+            for expr in expr_list {
+                result.push(eval_function(
+                    &func,
+                    vec![expr.eval()?])?)
+            }
+        },
+        _ => return Err(format!("Cannot map function on {:?}", data))
     }
     Ok(SExpr::Vec(result))
 }
