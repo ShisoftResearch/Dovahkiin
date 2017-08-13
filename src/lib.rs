39b74373a1aea7dfd6b96ab51570dc33c89438f6
Jack Shi <shisoftgenius@gmail.com>
2017-08-14 00:30:12 +0800

    scaffolding lisp parser

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 pub mod types;
 pub mod expr;
 pub mod lexer;
+pub mod parser;
 
 extern crate bincode;
 extern crate serde;
---
+++ src/parser/lisp.rs
@@ -0,0 +1,6 @@
+use expr::SExpr;
+use lexer::lisp::Token;
+
+pub fn parse_to_sexpr(tokens: Vec<Token>) -> Result<SExpr, String> {
+    unimplemented!();
+}
