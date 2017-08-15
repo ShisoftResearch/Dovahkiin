e4d61f49a5b9d2dad1e0388975afb29cdd1a23fd
Jack Shi <shisoftgenius@gmail.com>
2017-08-16 01:57:55 +0800

    cleanup test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,4 @@
 use dovahkiin::integrated::lisp;
-use dovahkiin::lexer::{lisp as lisp_lexer};
-use dovahkiin::parser::{lisp as lisp_parser};
 use dovahkiin::types::Value;
 use dovahkiin::expr::SExpr;
 
@@ -10,7 +8,6 @@ extern crate dovahkiin;
 pub fn lisp_integrated_plus_function() {
     let interpreter = lisp::get_interpreter();
     let str_function = " (+ -1i32 1i32 15i32)";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Value(Value::I32(15)));
 }
@@ -19,7 +16,6 @@ pub fn lisp_integrated_plus_function() {
 pub fn lisp_integrated_binding() {
     let interpreter = lisp::get_interpreter();
     let str_function = " (let [x 1u32] (+ 1u32 x))";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Value(Value::U32(2)));
 }
@@ -28,7 +24,6 @@ pub fn lisp_integrated_binding() {
 pub fn lisp_integrated_binding_2() {
     let interpreter = lisp::get_interpreter();
     let str_function = " (let [x 1u32 y 2u32] (+ x y))";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Value(Value::U32(3)));
 }
@@ -37,7 +32,6 @@ pub fn lisp_integrated_binding_2() {
 pub fn lisp_integrated_binding_def() {
     let interpreter = lisp::get_interpreter();
     let str_function = "(def x 1u32) (let [y 2u32] (+ x y))";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Value(Value::U32(3)));
 }
@@ -46,7 +40,6 @@ pub fn lisp_integrated_binding_def() {
 pub fn lisp_integrated_lambda() {
     let interpreter = lisp::get_interpreter();
     let str_function = " ((lambda [x] (+ 1u32 x)) 5u32)";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Value(Value::U32(6)));
 }
@@ -55,7 +48,6 @@ pub fn lisp_integrated_lambda() {
 pub fn lisp_integrated_lambda_2() {
     let interpreter = lisp::get_interpreter();
     let str_function = " ((lambda [x y] (* x y)) 5u32 4u32)";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Value(Value::U32(20)));
 }
@@ -64,7 +56,6 @@ pub fn lisp_integrated_lambda_2() {
 pub fn lisp_integrated_functional() {
     let interpreter = lisp::get_interpreter();
     let str_function = "(map (lambda [x] (+ x 1u32)) [1u32 2u32 3u32 4u32 5u32 6u32])";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Vec(vec![SExpr::Value(Value::U32(2)), SExpr::Value(Value::U32(3)),
                                SExpr::Value(Value::U32(4)), SExpr::Value(Value::U32(5)),
@@ -75,7 +66,6 @@ pub fn lisp_integrated_functional() {
 pub fn lisp_integrated_functional_symbolic() {
     let interpreter = lisp::get_interpreter();
     let str_function = " (map inc [1u32 2u32 3u32 4u32 5u32 6u32])";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Vec(vec![SExpr::Value(Value::U32(2)), SExpr::Value(Value::U32(3)),
                                SExpr::Value(Value::U32(4)), SExpr::Value(Value::U32(5)),
@@ -87,7 +77,6 @@ pub fn lisp_integrated_functional_defunc() {
     let interpreter = lisp::get_interpreter();
     let str_function = "(defunc dec [x] (- x 1u32))\
                              (map dec [1u32 2u32 3u32 4u32 5u32 6u32])";
-    let tokens = lisp_lexer::tokenize_str(str_function).unwrap();
     assert_eq!(lisp::eval_string(&interpreter, str_function).unwrap(),
                SExpr::Vec(vec![SExpr::Value(Value::U32(0)), SExpr::Value(Value::U32(1)),
                                SExpr::Value(Value::U32(2)), SExpr::Value(Value::U32(3)),
