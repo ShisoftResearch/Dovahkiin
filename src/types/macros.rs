7f891b7bc1b20fc6aba094bfd381f1c3b0626890
Hao Shi <shisoftgenius@gmail.com>
2018-06-18 00:48:46 +0800

    set default type id

--- src/types/macros.rs
+++ src/types/macros.rs
@@ -121,7 +121,8 @@ macro_rules! define_types {
             $(
                 $e = $id,
             )*
-            Map = 0 // No matter which id we pick for 'Map' because w/r planners will ignore it when sub_fields is not 'None'
+            Map = 1024, // No matter which id we pick for 'Map' because w/r planners will ignore it when sub_fields is not 'None',
+            Default = 0
         }
 
         $(
