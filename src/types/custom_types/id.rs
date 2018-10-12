70ce3210b6a03c1ab5ebd46bdc439ef4370f3b10
Hao Shi <shisoftgenius@gmail.com>
2018-10-13 04:28:56 +0800

    fix id under btree

--- src/types/custom_types/id.rs
+++ src/types/custom_types/id.rs
@@ -5,7 +5,7 @@ use byteorder::{ReadBytesExt, WriteBytesExt, BigEndian};
 use serde;
 use std::io::{Cursor, Error};
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize, Eq, Hash)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, Hash, Ord, PartialOrd, PartialEq, Eq)]
 pub struct Id {
     pub higher: u64,
     pub lower:  u64,
@@ -53,26 +53,66 @@ impl Id {
     }
 }
 
-impl PartialEq for Id {
-    fn eq(&self, other: &Id) -> bool {
-        self.higher == other.higher && self.lower == other.lower
-    }
-    fn ne(&self, other: &Id) -> bool {
-        self.higher != other.higher || self.lower != other.lower
-    }
-}
-impl PartialOrd for Id {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self == other {return Some(Ordering::Equal)}
-        if self.is_greater_than(other) {return Some(Ordering::Greater)}
-        Some(Ordering::Less)
-    }
-}
+#[cfg(test)]
+mod test {
+    use std::collections::BTreeMap;
+    use types::custom_types::id::Id;
+    use std::cmp::Ordering;
+    use std::collections::HashMap;
 
-impl Ord for Id {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self == other {return Ordering::Equal}
-        if self.is_greater_than(other) {return Ordering::Greater}
-        Ordering::Less
+    #[test]
+    fn compare() {
+        let id_1 = Id::new(1, 2);
+        let id_2 = Id::new(2, 2);
+        let id_3 = Id::new(1, 2);
+        let id_4 = Id::new(1, 3);
+        assert_eq!(id_1.cmp(&id_2), Ordering::Less);
+        assert!(id_1 < id_2);
+        assert_eq!(id_1, id_3);
+        assert!(id_4 > id_1);
+    }
+
+    #[test]
+    fn with_btree() {
+        let mut map = BTreeMap::new();
+        let id_1 = Id::new(1, 2);
+        let id_2 = Id::new(2, 2);
+        let id_3 = Id::new(1, 3);
+        let id_4 = Id::new(1, 5);
+        let id_5 = Id::new(3, 5);
+
+        assert!(map.insert(id_1, 1).is_none());
+        assert!(map.insert(id_2, 2).is_none());
+        assert!(map.insert(id_3, 3).is_none());
+        assert!(map.insert(id_4, 4).is_none());
+        assert!(map.insert(id_5, 5).is_none());
+
+        assert_eq!(map.get(&id_1), Some(&1));
+        assert_eq!(map.get(&id_2), Some(&2));
+        assert_eq!(map.get(&id_3), Some(&3));
+        assert_eq!(map.get(&id_4), Some(&4));
+        assert_eq!(map.get(&id_5), Some(&5));
+    }
+
+    #[test]
+    fn with_hashmap() {
+        let mut map = HashMap::new();
+        let id_1 = Id::new(1, 2);
+        let id_2 = Id::new(2, 2);
+        let id_3 = Id::new(1, 3);
+        let id_4 = Id::new(1, 5);
+        let id_5 = Id::new(3, 5);
+
+        assert!(map.insert(id_1, 1).is_none());
+        assert!(map.insert(id_2, 2).is_none());
+        assert!(map.insert(id_3, 3).is_none());
+        assert!(map.insert(id_4, 4).is_none());
+        assert!(map.insert(id_5, 5).is_none());
+
+        assert_eq!(map.get(&id_1), Some(&1));
+        assert_eq!(map.get(&id_2), Some(&2));
+        assert_eq!(map.get(&id_3), Some(&3));
+        assert_eq!(map.get(&id_4), Some(&4));
+        assert_eq!(map.get(&id_5), Some(&5));
     }
 }
